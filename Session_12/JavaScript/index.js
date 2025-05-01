89677811ec9fcbfecf567f9af8b5f5edb5f678f2
shreekarsharma <shreekarsharma@hotmail.com>
2025-05-01 16:05:17 +0530

    Learned about Global Execution Context, Temporal Dead Zone, Hoisting and Var, Let, Const

---
+++ Session_12/JavaScript/index.js
@@ -0,0 +1,17 @@
+// console.log(d);
+var a = 19;
+let b = 30;
+// console.log(c);
+const c = 40;
+function sum(a, b) {
+  console.log(a);
+  let a = 39;
+  console.log(a);
+  return a + b;
+}
+var d = "vikas";
+var res = sum(10, 20);
+console.log(res);
+console.log(a);
+console.log(b);
+console.log(c);
